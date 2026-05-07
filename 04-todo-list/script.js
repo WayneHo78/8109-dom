cca0cc9aacff3cad30b6042ca207e3a0f817c5c2
WayneHo78 <84441094+WayneHo78@users.noreply.github.com>
2026-05-07 03:36:23 +0000

    updated demo

--- 04-todo-list/script.js
+++ 04-todo-list/script.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
     displayTasks(tasks);
 
     const addTodoButton = document.querySelector("#addTodo");
-    addTodoButton.addEventListener("click", function(){
+    addTodoButton.addEventListener("click", function () {
         const name = document.querySelector("#name").value;
         const dueDate = document.querySelector("#dueDate").value;
         const urgency = document.querySelector("#urgency").value;
@@ -14,7 +14,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
         displayTasks(tasks);
 
-        
+
     })
 
 })
@@ -26,20 +26,84 @@ function displayTasks(tasks) {
     // remove all the existing tasks
     taskListUl.innerText = "";
 
+
     for (let t of tasks) {
         const liElement = document.createElement("li");
-        liElement.className = "list-group-item d-flex justify-content-between";
+        liElement.className = "list-group-item";
         // alternatively:
         // liElement.classList.add('list-group-item');
         // liElement.classList.add('d-flex');
         // liElement.classList.add('justify-content-between');
 
         liElement.innerHTML = `
-            <div>${t.name}</div>
-            <div>Date Due: ${t.dateDue}</div>
-            <div>Urgency: ${t.urgency}</div>
-        
+        <div class="row">
+            <div class="col-6">${t.name}</div>
+            <div class="col">Date Due: ${t.dateDue}</div>
+            <div class="col">Urgency: ${t.urgency}</div>
+            <div class="col">
+                <button class="m-2 btn btn-danger btn-sm delete-btn">Delete</button>
+                <button class="m-2 btn btn-success btn-sm update-btn">Update</button>
+            </div>
+        </div>
         `
+
+        // retrieve the delete button inside liElement
+        const deleteBtn = liElement.querySelector(".delete-btn");
+        deleteBtn.addEventListener("click", function () {
+            deleteTask(tasks, t.id);
+            displayTasks(tasks);
+        })
+
+        const updateBtn = liElement.querySelector(".update-btn");
+        // v1: plain version using prompt
+        // updateBtn.addEventListener("click", function(){
+        //     const newName = prompt("Enter the new name", t.name);
+        //     const newDate = prompt("Enter the new date", t.dateDue);
+        //     const newUrgency = prompt("Enter the new urgency", t.urgency);
+        //     updateTask(tasks, t.id, newName, newDate, newUrgency);
+        //     displayTasks(tasks);
+        // })
+
+        // v2: using SweetAlert
+        updateBtn.addEventListener("click", function () {
+            Swal.fire({
+                "title": `Update task: ${t.name}`,
+                "html": `
+                    <div>
+                        <div class="m-2">
+                            <label>Task Name</label>
+                            <input type="text" id="newName" class="form-control" value="${t.name}" />
+                        </div>
+                        <div class="m-2">
+                            <label>Date Due</label>
+                            <input type="date" id="newDueDate" class="form-control" value="${t.dateDue}"/>
+                        </div>
+                        <div class="m-2">
+                            <label>Urgency</label>
+                            <select id="newUrgency" class="form-control">
+                            <option value="1" ${t.urgency == 1 ? "selected" : ""}>1</option>
+                            <option value="2" ${t.urgency == 2 ? "selected" : ""}>2</option>
+                            <option value="3" ${t.urgency == 3 ? "selected" : ""}>3</option>
+                            <option value="4" ${t.urgency == 4 ? "selected" : ""}>4</option>
+                            <option value="5" ${t.urgency == 5 ? "selected" : ""}>5</option>
+                            </select>
+
+                        </div>
+                    </div>
+                                    `,
+                showCancelButton: true,
+                showCloseButton: true,
+                preConfirm: function() {
+                    // preConfirm is called when the user pressed on the confirm button
+                    let newTaskName = document.querySelector("#newName").value;
+                    let newDateDue = document.querySelector("#newDueDate").value;
+                    let newUrgency = document.querySelector("#newUrgency").value;
+                    
+                    updateTask(tasks, t.id, newTaskName, newDateDue, newUrgency);
+                    displayTasks(tasks);
+                }
+            });
+        })
 
         taskListUl.appendChild(liElement);
     }
